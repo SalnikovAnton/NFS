1ac546c05a9d233ad24f94892db746f66f39b569
ANTON SALNIKOV <73490915+SalnikovAnton@users.noreply.github.com>
2023-01-29 18:26:58 +0300

    Update nfsc_script.sh

--- nfsc_script.sh
+++ nfsc_script.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-sudo yum install -y nfs-utils
+yum install -y nfs-utils
 systemctl enable firewalld --now 
-sudo firewall-cmd --add-port=2049/udp
-sudo mkdir -p /mnt
-sudo echo "192.168.50.10:/srv/share/ /mnt nfs vers=3,proto=udp,noauto,x-systemd.automount 0 0" >> /etc/fstab 
-sudo systemctl daemon-reload 
-sudo systemctl restart remote-fs.target 
+mkdir -p /mnt
+echo "192.168.50.10:/srv/share/ /mnt nfs vers=3,proto=udp,noauto,x-systemd.automount 0 0" >> /etc/fstab 
+systemctl daemon-reload 
+systemctl restart remote-fs.target 
