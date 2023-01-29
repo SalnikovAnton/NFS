050bf72b835c679743f2a243b320723a63709eb5
ANTON SALNIKOV <73490915+SalnikovAnton@users.noreply.github.com>
2023-01-29 18:27:44 +0300

    Update nfss_script.sh

--- nfss_script.sh
+++ nfss_script.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-sudo yum install -y nfs-utils
-sudo systemctl enable firewalld --now
-sudo firewall-cmd --add-service="nfs3" 
-sudo firewall-cmd --add-service="rpc-bind" 
-sudo firewall-cmd --add-service="mountd" 
-sudo firewall-cmd --add-port=2049/udp
-sudo firewall-cmd --permanent 
-sudo firewall-cmd --reload 
-sudo systemctl enable nfs --now
-sudo mkdir -p /srv/share/upload 
-sudo chown -R nfsnobody:nfsnobody /srv/share 
-sudo chmod 0777 /srv/share/upload 
-sudo cat << EOF > /etc/exports 
+yum install -y nfs-utils
+systemctl enable firewalld --now
+firewall-cmd --add-service=nfs3 --add-service=rpc-bind --add-service=mountd --permanent
+firewall-cmd --reload 
+systemctl enable nfs --now
+mkdir -p /srv/share/upload 
+chown -R nfsnobody:nfsnobody /srv/share 
+chmod 0777 /srv/share/upload 
+cat << EOF > /etc/exports 
 /srv/share 192.168.50.11/24(rw,sync,root_squash) 
 EOF
-sudo exportfs -r 
+exportfs -r 
